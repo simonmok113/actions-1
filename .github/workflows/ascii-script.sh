53153055f90130f1b4b1e8cc42055f132406a314
simonmok113 <88585739+simonmok113@users.noreply.github.com>
2025-11-17 04:11:03 +0800

    added repo level secrets

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh 
-sudo apt-get install -y cowsay
-cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -ltra
+#sudo apt-get install -y cowsay
+#cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+#grep -i "dragon" dragon.txt
+#cat dragon.txt
+#ls -ltra
